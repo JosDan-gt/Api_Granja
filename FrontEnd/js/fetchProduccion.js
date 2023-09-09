9844a698ae71824afb726ae718e6c5fa85f4a7b8
JosDan-gt <jaldanaa3@miumg.edu.gt>
2023-09-08 23:18:07 -0600

    Agreglos

--- FrontEnd/js/fetchProduccion.js
+++ FrontEnd/js/fetchProduccion.js
@@ -65,49 +65,49 @@ function verProduccion(id) {
 
     var newUrl = url2 + '/' + id;
     //console.log(newUrl)
-    
+
     fetch(newUrl)
-    .then(response2 => {
-        if(!response2.ok){
-            throw new Error(response2.statusText)
-        }
-        return response2.json()
-    })
-    .then(data2 => {
-        console.log(data2)
+        .then(response2 => {
+            if (!response2.ok) {
+                throw new Error(response2.statusText)
+            }
+            return response2.json()
+        })
+        .then(data2 => {
+            console.log(data2)
 
-        var huevosDia = document.getElementById('huevosDia')
-        huevosDia.value = data2.Huevos_PorDia
+            var huevosDia = document.getElementById('huevosDia')
+            huevosDia.value = data2.Huevos_PorDia
 
-        var cantPeque = document.getElementById('cantPeque')
-        cantPeque.value = data2.CantidadCartones_Pequeno
+            var cantPeque = document.getElementById('cantPeque')
+            cantPeque.value = data2.CantidadCartones_Pequeno
 
-        var cantMed = document.getElementById('cantMed')
-        cantMed.value = data2.CantidadCartones_Mediano
+            var cantMed = document.getElementById('cantMed')
+            cantMed.value = data2.CantidadCartones_Mediano
 
-        var cantGrand = document.getElementById('cantGrand')
-        cantGrand.value = data2.CantidadCartones_Grande
+            var cantGrand = document.getElementById('cantGrand')
+            cantGrand.value = data2.CantidadCartones_Grande
 
-        var cantJumb = document.getElementById('cantJumb')
-        cantJumb.value = data2.CantidadCartones_Jumbo
+            var cantJumb = document.getElementById('cantJumb')
+            cantJumb.value = data2.CantidadCartones_Jumbo
 
-        var cantGall = document.getElementById('cantGall')
-        cantGall.value = data2.Cantidad_Gallinas
+            var cantGall = document.getElementById('cantGall')
+            cantGall.value = data2.Cantidad_Gallinas
 
-        var cantCajas = document.getElementById('cantCajas')
-        cantCajas.value = data2.Cantidad_Cajas
+            var cantCajas = document.getElementById('cantCajas')
+            cantCajas.value = data2.Cantidad_Cajas
 
-        var cantPerd = document.getElementById('cantPerd')
-        cantPerd.value = data2.Cantidad_Perdida
-        
-        var fecha = document.getElementById('fecha2')
-        fecha.value = data2.Fecha_Control
-        
-    })
-    .catch(err => {
-        console.log(err)
-    })
-    
+            var cantPerd = document.getElementById('cantPerd')
+            cantPerd.value = data2.Cantidad_Perdida
+
+            var fecha = document.getElementById('fecha2')
+            fecha.value = data2.Fecha_Control
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
 }
 
 
@@ -116,106 +116,110 @@ function verProduccionmod(id) {
 
     var newUrl = url2 + '/' + id
     console.log(newUrl)
-    
+
     fetch(newUrl)
-    .then(response2 => {
-        if(!response2.ok){
-            throw new Error(response2.statusText)
-        }
-        return response2.json()
-    })
-    .then(data2 => {
-        console.log(data2)
+        .then(response2 => {
+            if (!response2.ok) {
+                throw new Error(response2.statusText)
+            }
+            return response2.json()
+        })
+        .then(data2 => {
+            console.log(data2)
 
-        var huevosDia = document.getElementById('huevosDiaMod')
-        huevosDia.value = data2.Huevos_PorDia
+            var huevosDia = document.getElementById('huevosDiaMod')
+            huevosDia.value = data2.Huevos_PorDia
 
-        var cantPeque = document.getElementById('cantPequeMod')
-        cantPeque.value = data2.CantidadCartones_Pequeno
+            var cantPeque = document.getElementById('cantPequeMod')
+            cantPeque.value = data2.CantidadCartones_Pequeno
 
-        var cantMed = document.getElementById('cantMedMod')
-        cantMed.value = data2.CantidadCartones_Mediano
+            var cantMed = document.getElementById('cantMedMod')
+            cantMed.value = data2.CantidadCartones_Mediano
 
-        var cantGrand = document.getElementById('cantGrandMod')
-        cantGrand.value = data2.CantidadCartones_Grande
+            var cantGrand = document.getElementById('cantGrandMod')
+            cantGrand.value = data2.CantidadCartones_Grande
 
-        var cantJumb = document.getElementById('cantJumbMod')
-        cantJumb.value = data2.CantidadCartones_Jumbo
+            var cantJumb = document.getElementById('cantJumbMod')
+            cantJumb.value = data2.CantidadCartones_Jumbo
 
-        var cantGall = document.getElementById('cantGallMod')
-        cantGall.value = data2.Cantidad_Gallinas
+            var cantGall = document.getElementById('cantGallMod')
+            cantGall.value = data2.Cantidad_Gallinas
 
-        var cantCajas = document.getElementById('cantCajasMod')
-        cantCajas.value = data2.Cantidad_Cajas
+            var cantCajas = document.getElementById('cantCajasMod')
+            cantCajas.value = data2.Cantidad_Cajas
 
-        var cantPerd = document.getElementById('cantPerdMod')
-        cantPerd.value = data2.Cantidad_Perdida
-        
-        var fecha = document.getElementById('fechaMod')
-        fecha.value = data2.Fecha_Control
+            var cantPerd = document.getElementById('cantPerdMod')
+            cantPerd.value = data2.Cantidad_Perdida
 
-        produccion.Huevos_PorDia = data2.Huevos_PorDia
-        produccion.CantidadCartones_Pequeno = data2.CantidadCartones_Pequeno
-        produccion.CantidadCartones_Mediano = data2.CantidadCartones_Mediano
-        produccion.CantidadCartones_Grande = data2.CantidadCartones_Grande
-        produccion.CantidadCartones_Jumbo = data2.CantidadCartones_Jumbo
-        produccion.Cantidad_Gallinas = data2.Cantidad_Gallinas
-        produccion.Cantidad_Cajas = data2.Cantidad_Cajas
-        produccion.Cantidad_Perdida = data2.Cantidad_Perdida
-        produccion.Fecha_Control = data2.Fecha_Control
+            var fecha = document.getElementById('fechaMod')
+            fecha.value = data2.Fecha_Control
 
-        
-    })
-    .catch(err => {
-        console.log(err)
-    })
-    
+            produccion.DiaControl_Numero = data2.DiaControl_Numero
+            produccion.Huevos_PorDia = data2.Huevos_PorDia
+            produccion.CantidadCartones_Pequeno = data2.CantidadCartones_Pequeno
+            produccion.CantidadCartones_Mediano = data2.CantidadCartones_Mediano
+            produccion.CantidadCartones_Grande = data2.CantidadCartones_Grande
+            produccion.CantidadCartones_Jumbo = data2.CantidadCartones_Jumbo
+            produccion.Cantidad_Gallinas = data2.Cantidad_Gallinas
+            produccion.Cantidad_Cajas = data2.Cantidad_Cajas
+            produccion.Cantidad_Perdida = data2.Cantidad_Perdida
+            produccion.Fecha_Control = data2.Fecha_Control
+
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
 }
 
 
-function modificarProduccion(){
+function modificarProduccion() {
 
     var newUrl = url2 + '/' + produccion.DiaControl_Numero
 
     console.log(JSON.stringify(produccion))
     console.log(newUrl)
 
-    // var nombreEmpleado = document.getElementById('nombreEmpleadoMod').value
-    // var apellidoEmpleado = document.getElementById('apellidoEmpleadoMod').value
-    // var direccionEmpleado = document.getElementById('direccionEmpleadoMod').value
-    // var puestoEmpleado = document.getElementById('puestoEmpleadoMod').value
-    // var edadEmpleado = document.getElementById('edadEmpleadoMod').value
-    // var telefonoEmpleado = document.getElementById('telefonoEmpleadoMod').value
-    // var salarioEmpleado = document.getElementById('salarioEmpleadoMod').value
+    var huevosDia = document.getElementById('huevosDiaMod').value
+    var cantPeque = document.getElementById('cantPequeMod').value
+    var cantMed = document.getElementById('cantMedMod').value
+    var cantGrand = document.getElementById('cantGrandMod').value
+    var cantJumb = document.getElementById('cantJumbMod').value
+    var cantGall = document.getElementById('cantGallMod').value
+    var cantCajas = document.getElementById('cantCajasMod').value
+    var cantPerd = document.getElementById('cantPerdMod').value
+    var fecha = document.getElementById('fechaMod').value
 
-    // empleadoFinal = {
-    //     "Id_Empleado": empleado.Id_Empleado,
-    //     "Nombre_Empleado": nombreEmpleado,
-    //     "Apellido_Empleado": apellidoEmpleado,
-    //     "Direccion_Empleado": direccionEmpleado,
-    //     "Puesto_Empleado": puestoEmpleado,
-    //     "Edad_Empleado": edadEmpleado,
-    //     "Telefono_Empleado": telefonoEmpleado,
-    //     "Salario_Empleado": salarioEmpleado,
-    //     "Solvencia_Salario": empleado.Solvencia_Salario
-    //}
+    produccionFinal = {
+        "DiaControl_Numero": produccion.DiaControl_Numero,
+        "Huevos_PorDia": huevosDia,
+        "CantidadCartones_Pequeno": cantPeque,
+        "CantidadCartones_Mediano": cantMed,
+        "CantidadCartones_Grande": cantGrand,
+        "CantidadCartones_Jumbo": cantJumb,
+        "Cantidad_Gallinas": cantGall,
+        "Cantidad_Cajas": cantCajas,
+        "Cantidad_Perdida": cantPerd,
+        "Fecha_Control": fecha
+    }
 
-    // fetch(newUrl, {
-    //     method: 'PUT',
-    //     body: JSON.stringify(empleadoFinal),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // })
-    // .then(res => res.json())
-    // .then(data => {
+    fetch(newUrl, {
+        method: 'PUT',
+        body: JSON.stringify(produccionFinal),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data2 => {
 
-    //     alert(data)
-    //     getEmpleados()
+            alert(data2)
+            getProduccion()
 
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error)
-    // })
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        })
 
 }
